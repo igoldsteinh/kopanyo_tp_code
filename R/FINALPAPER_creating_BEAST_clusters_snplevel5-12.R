0e18be2eda674df6ee12ddeb9b9bd79928cfdf62
igoldsteinh <61714888+igoldsteinh@users.noreply.github.com>
2022-01-28 15:38:55 -0800

    added all files and code needed for snp10 analysis

---
+++ R/FINALPAPER_creating_BEAST_clusters_snplevel5-12.R
@@ -0,0 +1,297 @@
+# This file is for splitting fasta files into cluster groupings and renaming them for input into BEAST
+
+library(tidyverse)
+library(ape)
+library(transcluster)
+library(phylotools)
+library(treedater)
+library(lubridate)
+library(here)
+library(aplot)
+library(ggtree)
+library(xtable)
+set.seed(1)
+
+
+# reading in data ---------------------------------------------------------
+
+
+samples1 <- read.dna(file=here("data", "Botswana_lineage_1_86_samples.fasta"), 
+                     format = "fasta") %>% 
+  `rownames<-`(str_extract(labels(.), "^BTB-\\d+"))
+
+samples2 <- read.dna(file=here("data", "Botswana_lineage_2_76_samples.fasta"), 
+                     format = "fasta") %>% 
+  `rownames<-`(str_extract(labels(.), "^BTB-\\d+"))
+
+samples3 <- read.dna(file=here("data", "Botswana_lineage_3_12_samples.fasta"), 
+                     format = "fasta") %>% 
+  `rownames<-`(str_extract(labels(.), "^BTB-\\d+"))
+
+samples4 <- read.dna(file=here("data", "Botswana_lineage_4_1252_samples.fasta"), 
+                     format = "fasta") %>% 
+  `rownames<-`(str_extract(labels(.), "^BTB-\\d+"))
+
+
+
+meta_name <- "Botswana_1426_good_quality_resistance_metadata.csv"
+
+meta_data <- read_csv2(here("data", meta_name)) %>%
+  filter(gMixture < 2) %>%
+  filter(!is.na(collectdt))
+
+num_samples <- dim(meta_data)[1]
+
+samples <- list(samples1, samples2, samples3, samples4)
+
+
+samples <- map(samples, ~.[rownames(.) %in% meta_data$SampleID,])
+
+meta_data <- meta_data %>%
+  mutate(lineage_rough = str_sub(Lineage_detailed,1, 1)) %>%
+  split(.$lineage_rough)
+
+
+# clustering --------------------------------------------------------------
+# snp matrix from 2021_03_12_1426_sample_clusters_and_snps
+# actual code 
+# snp_matrix <- map(samples, dist.gene, method = "pairwise")%>%
+# map(as.matrix)
+
+snp_matrix <- read_rds(here::here("code", 
+                                  "R Code", 
+                                  str_c(as.character(num_samples), 
+                                        "_samples_snp_matrix_list.rds")))
+ids <- map(samples, rownames)
+
+lambda <- 1.5
+beta <- 2.0
+
+dates <- map(meta_data, ~lubridate::year(.x$collectdt))
+
+my_model <- pmap(list(ids, dates, snp_matrix), createModel)
+# pick which SNP cutoffs to use
+my_model <- map(my_model, setSNPThresholds,c(5,10))
+# pick which transmission cutoffs to use
+my_model <- map(my_model, setTransThresholds, seq(4, 24, by=2))
+# choose clock rate (lambda) and transmission rate (beta)
+my_model <- map(my_model, setParams, lambda = lambda, beta = beta) 
+
+my_SNP_clusters <- map(my_model, makeSNPClusters, writeFile = F)
+# my_trans_clusters <- map(my_model, makeTransClusters, writeFile = F)
+
+snp_levels <- c(5, 6,7,8,9,10,11,12)
+id <- meta_data$SampleID
+
+ids <- map(samples, rownames)
+
+
+id_list_snp <- map(ids, list) %>%
+  map(rep, times = 8)
+snp_frame <- map2(my_SNP_clusters, id_list_snp, ~map2(.x, .y, data.frame))
+snp_frame <- map(snp_frame, bind_rows, .id="id")%>%
+  map(~.x %>%
+        rename("cluster"= ".x..i..",
+               "snp_level"="id",
+               "id" = ".y..i.."))
+
+
+# splitting samples up into clusters at different snp levels ---------------
+# I apologize to Mine but I'm lazy and I'm just going to loop it
+snp_levels<- c(5, 6,7,8,9,10,11,12)
+
+samples1 <- read.fasta(file=here("data", "Botswana_lineage_1_86_samples.fasta"))
+
+samples2 <- read.fasta(file=here("data", "Botswana_lineage_2_76_samples.fasta"))
+
+
+samples3 <- read.fasta(file=here("data", "Botswana_lineage_3_12_samples.fasta"))
+
+samples4 <- read.fasta(file=here("data", "Botswana_lineage_4_1252_samples.fasta"))
+
+for (i in 1: length(snp_levels)) {
+  level <- snp_levels[i]
+  snp_level <- str_c("snp", level, sep="")
+  
+  snp_frame_level <- map(snp_frame, ~.x %>%
+                     filter(snp_level == level) %>%
+                     group_by(cluster) %>% 
+                     mutate(count = n()) %>% 
+                     filter(count >= 4))
+  
+  samples_data_frame <- list(samples1, samples2, samples3, samples4)
+  
+  
+  id_times <- map(meta_data, ~.x %>%
+                    dplyr::select(SampleID, collectdt, lineage_rough) %>%
+                    mutate(date_id = str_c(SampleID, as.character(collectdt), sep = "_")))
+  
+  
+  
+  samples_data_frame <- pmap(list(samples_data_frame, id_times, snp_frame_level), ~..1 %>% 
+                               left_join(..2, by = c("seq.name" = "SampleID")) %>%
+                               filter(seq.name %in% ..3$id) %>%
+                               dplyr::select(date_id, seq.text) %>%
+                               rename("seq.name" = "date_id"))
+  
+  
+  
+  cluster_names <- map2(snp_frame_level, id_times, ~.x %>%
+                          dplyr::select(id, cluster) %>%
+                          left_join(.y, by = c("id" = "SampleID")) %>%
+                          rename("seq.name" = "date_id") %>%
+                          mutate(cluster_name = str_c(snp_level,
+                                                      "lineage", 
+                                                      lineage_rough, 
+                                                      "cluster", 
+                                                      cluster, 
+                                                      sep = "_")) %>%
+                          ungroup() %>%
+                          dplyr::select(seq.name, cluster_name))
+  
+  map2(samples_data_frame, cluster_names, split_dat)
+  
+  match <- str_c("^snp", level, sep="")
+  file_list <- list.files("C:/Users/fiddl/Documents/kopanyo-archived-phylo/", pattern = match)
+  new_samples <- map(file_list, ~read.dna(here::here(.x), format = "fasta"))
+  
+  var_sites <- map(new_samples, ~seg.sites(.x))
+  
+  samples_varsites <- map2(new_samples, var_sites, ~.x[,.y])
+  
+  var_sites_num <- map(var_sites, length) %>% 
+    unlist()
+  
+
+  #cluster dictionary
+  dictionary <- data.frame(unlist(file_list), var_sites_num) %>% 
+    rename("file_name" = "unlist.file_list.")
+  
+  cluster_counts <- map2(snp_frame_level, id_times, ~.x %>%
+                           dplyr::select(id, cluster, count) %>%
+                           left_join(.y, by = c("id" = "SampleID")) %>%
+                           rename("seq.name" = "date_id") %>%
+                           mutate(cluster_name = str_c(snp_level,
+                                                       "lineage", 
+                                                       lineage_rough, 
+                                                       "cluster", 
+                                                       cluster, 
+                                                       sep = "_")) %>%
+                           ungroup() %>%
+                           dplyr::select(cluster_name, count) %>%
+                           unique()) %>% 
+    bind_rows() %>% 
+    mutate(file_name = str_c(cluster_name, ".fasta", sep=""))
+  
+  
+  dictionary <- dictionary %>% 
+    left_join(cluster_counts, by = "file_name") %>%
+    mutate(snp_level = level)
+  
+  write_csv(dictionary, here::here("data", str_c("snp", level, "kopanyo_cluster_dictionary.csv", sep = "_")))
+}
+
+# splitting by SNP level five ---------------------------------------------
+
+snp_frame_five <- map(snp_frame, ~.x %>%
+                                 filter(snp_level == 5) %>%
+                                 group_by(cluster) %>% 
+                                 mutate(count = n()) %>% 
+                                 filter(count >= 4))
+
+samples_five <- map2(samples, snp_frame_five,  ~.x[rownames(.x) %in% .y$id,])
+
+
+
+
+#let's do the whole whammy here, let's rename the sequences with their sample dates and then also grab 
+
+samples1 <- read.fasta(file=here("data", "Botswana_lineage_1_86_samples.fasta"))
+
+samples2 <- read.fasta(file=here("data", "Botswana_lineage_2_76_samples.fasta"))
+
+
+samples3 <- read.fasta(file=here("data", "Botswana_lineage_3_12_samples.fasta"))
+
+samples4 <- read.fasta(file=here("data", "Botswana_lineage_4_1252_samples.fasta"))
+
+
+samples_data_frame <- list(samples1, samples2, samples3, samples4)
+
+
+id_times <- map(meta_data, ~.x %>%
+                            dplyr::select(SampleID, collectdt, lineage_rough) %>%
+                            mutate(date_id = str_c(SampleID, as.character(collectdt), sep = "_")))
+
+
+
+samples_data_frame <- pmap(list(samples_data_frame, id_times, snp_frame_five), ~..1 %>% 
+                             left_join(..2, by = c("seq.name" = "SampleID")) %>%
+                             filter(seq.name %in% ..3$id) %>%
+                             dplyr::select(date_id, seq.text) %>%
+                             rename("seq.name" = "date_id"))
+                           
+                           
+                           
+cluster_names <- map2(snp_frame_five, id_times, ~.x %>%
+                        dplyr::select(id, cluster) %>%
+                        left_join(.y, by = c("id" = "SampleID")) %>%
+                        rename("seq.name" = "date_id") %>%
+                        mutate(cluster_name = str_c("snp5", "lineage", lineage_rough, "cluster", cluster, sep = "_")) %>%
+                        ungroup() %>%
+                        dplyr::select(seq.name, cluster_name))
+
+map2(samples_data_frame, cluster_names, split_dat)
+
+
+# reducing down to varying sites ------------------------------------------
+file_list <- list.files("C:/Users/fiddl/Documents/kopanyo-archived-phylo/data/", pattern = "^snp5")
+new_samples <- map(file_list, ~read.dna(here::here("data", .x), format = "fasta"))
+
+var_sites <- map(new_samples, ~seg.sites(.x))
+
+
+test_samples <- new_samples[[1]]
+
+test_var <- var_sites[[1]]
+
+testing <- test_samples[,test_var]
+samples_varsites <- map2(new_samples, var_sites, ~.x[,.y])
+
+var_sites_num <- map(var_sites, length) %>% 
+                 unlist()
+
+quantile(var_sites_num)
+
+
+cutoff <- var_sites_num[var_sites_num >8]
+
+test <- read.dna(here::here("data", file_list[[39]]), format = "fasta")
+
+
+final_samples <- samples_varsites[-18]
+final_file_list <- file_list[-18]
+
+write.dna()
+map2(final_file_list, final_samples,
+     ~write.dna(.y, here::here("data", str_c("varsites", .x, sep = "_")), format = "fasta"))
+
+#cluster dictionary
+dictionary <- data.frame(unlist(file_list), var_sites_num) %>% 
+              rename("file_name" = "unlist.file_list.")
+
+cluster_counts <- map2(snp_frame_five, id_times, ~.x %>%
+                        dplyr::select(id, cluster, count) %>%
+                        left_join(.y, by = c("id" = "SampleID")) %>%
+                        rename("seq.name" = "date_id") %>%
+                        mutate(cluster_name = str_c("snp5", "lineage", lineage_rough, "cluster", cluster, sep = "_")) %>%
+                        ungroup() %>%
+                        dplyr::select(cluster_name, count) %>%
+                        unique()) %>% 
+                   bind_rows() %>% 
+                   mutate(file_name = str_c(cluster_name, ".fasta", sep=""))
+
+dictionary <- dictionary %>% 
+              left_join(cluster_counts, by = "file_name")
+
+write_csv(dictionary, here::here("data", "kopanyo_cluster_dictionary.csv"))
---
+++ R/snp10_kopanyo_archive_tp_analysis.R
@@ -0,0 +1,425 @@
+# repeating analysis of kopanyo archive data
+# using a 10 snp cutoff rather than a 5 snp
+# putting all R code into one place
+# beast code will be in separate files
+library(ape)
+library(tidyverse)
+library(TransPhylo)
+library(coda)
+library(rstan)
+library(tidybayes)
+library(lubridate)
+library(SAMBA)
+set.seed(1)
+
+# dictionaries and varfiles formed in creating_BEAST_clusters_snplevel5-12.R
+# selecting clusters for analysis -----------------------------------------
+
+# # dict10 <-read_csv(here::here("data", "snp_10_kopanyo_cluster_dictionary.csv")) %>%
+#          mutate(cuttoff = count * 2 -2, 
+#                 include = var_sites_num >= cuttoff) %>%
+#          filter(include == TRUE) %>%
+#          mutate(clock_start = (4.2E6/var_sites_num) * 1E-7,
+#                 clock_lb = (4.2E6/var_sites_num) * 1E-8,
+#                 clock_ub = (4.2E6/var_sites_num) * 5E-7)
+
+# write_csv(dict10,here::here("data", "snp_10_kopanyo_cluster_dictionary.csv") )
+# reduce to varsites ------------------------------------------------------
+# dict10 <-read_csv(here::here("data", "snp_10_kopanyo_cluster_dictionary.csv"))
+# 
+# file_list <- dict10$file_name
+# new_samples <- map(file_list, ~read.dna(here::here(.x), format = "fasta"))
+# 
+# var_sites <- map(new_samples, ~seg.sites(.x))
+# 
+# 
+# test_samples <- new_samples[[1]]
+# 
+# test_var <- var_sites[[1]]
+# 
+# testing <- test_samples[,test_var]
+# samples_varsites <- map2(new_samples, var_sites, ~.x[,.y])
+# 
+# var_sites_num <- map(var_sites, length) %>% 
+#   unlist()
+# 
+# map2(file_list, samples_varsites,
+#      ~write.dna(.y, here::here("data", str_c("varsites", .x, sep = "_")), format = "fasta"))
+
+
+# read in mcctrees --------------------------------------------------------
+dict10 <-read_csv(here::here("data", "snp_10_kopanyo_cluster_dictionary.csv"))
+
+mcc_name <- str_c("mcctree", dict10$file_name, sep = "_")
+
+mcc_file_list <- str_replace(mcc_name,"\\..*","") 
+time_tree_list <- map(mcc_file_list, ~read.nexus(here::here("BEAST2", .x)))
+
+# extract sample dates by group
+# fine max date in each sample
+sample_names <- map(time_tree_list, pluck, "tip.label") %>%
+                map(data.frame) %>%
+                bind_rows(.id = "tree") %>%
+                rename("sample_name" = ".x..i..")
+
+
+file_name <- data.frame(tree = 1:46, file_name = dict10$file_name)
+
+sample_names$tree <- as.numeric(sample_names$tree)
+
+sample_names <- sample_names %>%
+                left_join(file_name, by = "tree") %>%
+                mutate(date = str_sub(sample_name, start = -10))
+
+sample_names$date <- as.Date(sample_names$date)
+
+max_dates <- sample_names %>%
+             group_by(file_name) %>%
+             summarise(max_date = max(date))
+
+date_last_sample <- lubridate::decimal_date(max_dates$max_date)
+
+p_tree_list <- map2(time_tree_list, date_last_sample,
+                    ptreeFromPhylo)
+
+
+
+# analysis sharing both off.r and neg ---------------------------
+# rescale these to years
+# generation time gamma distribution parameters
+w.shape = 10
+w.scale = 1/10
+
+# sampling time gamma distribution parameters
+ws.shape = 10
+ws.scale = 1/10
+
+# infer the transmission tree
+# for real analysis, change mcmcIterations to 100000
+res10 <- infer_multittree_share_param(p_tree_list,
+                                    mcmcIterations = 100,
+                                    thinning = 10,
+                                    w.shape = w.shape,
+                                    w.scale = w.scale,
+                                    ws.shape = ws.shape,
+                                    ws.scale = ws.scale,
+                                    startOff.p = .5,
+                                    updateOff.p = FALSE,
+                                    prior_pi_a = 1,
+                                    prior_pi_b = 19,
+                                    share = c("neg", "off.r"),
+                                    startNeg = 1.48,
+                                    dateT = max(date_last_sample) + .01)
+
+# write_rds(res, "snp10_kopanyo_archive_tp_allshare_res.rds")
+
+
+# convergence diagnostics for all share -----------------------------------
+
+res10 <- read_rds(here::here("snp10_kopanyo_archive_tp_allshare_res.rds"))
+
+get_param_estimates <- function(record, p){
+  sapply(record, function(x) x[[p]])
+}
+
+
+mean3_full_param_results10 <- map(res10, ~data.frame(pi = get_param_estimates(., "pi"),
+                                                 off.r = get_param_estimates(., "off.r"),
+                                                 neg = get_param_estimates(., "neg"),
+                                                 pTTree = get_param_estimates(., "pTTree"),
+                                                 pPTree = get_param_estimates(., "pPTree"))) %>%
+  map(~.x %>% mutate(log_posterior = pTTree + pPTree))
+
+# ptree_plots <- map(mean3_full_param_results, ~plot(.$pPTree, type = "l"))
+# ttree_plots <- map(mean3_full_param_results, ~plot(.$pTTree, type = "l"))
+off.r_plots <- map(mean3_full_param_results10, ~plot(.$off.r, type = "l"))
+neg_plots <- map(mean3_full_param_results10, ~plot(.$neg, type = "l"))
+# pi_plots <- map(mean3_full_param_results10, ~plot(.$pi, type = "l"))
+
+lp_plots <-map(mean3_full_param_results10, ~plot(.$log_posterior, type = "l"))
+
+
+#pull effs
+mean3_coda_res <- map(res10, convertToCoda)
+
+mean3_eff <- map(mean3_coda_res, effectiveSize) %>%
+  bind_rows(.id = "column_label")
+
+
+# create dat for analysis all share ---------------------------------------
+
+
+dict10 <-read_csv(here::here("data", "snp_10_kopanyo_cluster_dictionary.csv"))
+
+mcc_name <- str_c("mcctree", dict10$file_name, sep = "_")
+
+mcc_file_list <- str_replace(mcc_name,"\\..*","") 
+time_tree_list <- map(mcc_file_list, ~read.nexus(here::here("code", "BEAST2", .x)))
+
+# extract sample dates by group
+# fine max date in each sample
+sample_names <- map(time_tree_list, pluck, "tip.label") %>%
+  map(data.frame) %>%
+  bind_rows(.id = "tree") %>%
+  rename("sample_name" = ".x..i..")
+
+
+file_name <- data.frame(tree = 1:46, file_name = dict10$file_name)
+
+sample_names$tree <- as.numeric(sample_names$tree)
+
+sample_names <- sample_names %>%
+  left_join(file_name, by = "tree") %>%
+  mutate(date = str_sub(sample_name, start = -10))
+
+sample_names$date <- as.Date(sample_names$date)
+
+max_dates <- sample_names %>%
+  group_by(file_name) %>%
+  summarise(max_date = max(date))
+
+date_last_sample <- lubridate::decimal_date(max_dates$max_date)
+
+p_tree_list <- map2(time_tree_list, date_last_sample,
+                    ptreeFromPhylo)
+
+
+
+names10 <- map(res10, pluck, 1, "ctree", "nam")
+offspring10 <- map2(res10, names10, getOffspringDist, burnin = .5) 
+
+prob_source10 <- map(offspring10, ~1-rowSums(. == 0)/dim(.)[2])
+
+prob_inf_frame10 <- map2(names10, prob_source10, data.frame)%>%
+  map(~.x %>% rename("names" =".x..i..", "prob_source" = ".y..i.."))%>%
+  map(~.x %>% mutate(new_names = str_replace(.$names, ".*_", "_"))) %>%
+  map(~.x %>% mutate(final_names = str_replace_all(.$new_names, "_", ""),
+                     tp_source = prob_source > .6)) %>%
+  bind_rows(.id = "id")
+
+hist(prob_inf_frame10$prob_source)
+
+
+meta_name <- "Botswana_1426_good_quality_resistance_metadata.csv"
+
+meta_data <- read_csv2(here::here("data", meta_name)) %>%
+  filter(gMixture < 2) %>%
+  filter(!is.na(collectdt))
+
+
+hiv_status <- meta_data %>%
+  dplyr::select(SampleID, hivfinal_new)
+
+id_crosswalk <- sample_names %>%
+  mutate(SampleID = str_sub(sample_name, end = -12)) %>%
+  rename("seq.name" = "sample_name") %>%
+  dplyr::select(seq.name, SampleID)
+
+prob_inf_frame10 <- prob_inf_frame10 %>%
+  left_join(id_crosswalk, by= c("names" = "seq.name")) %>%
+  left_join(hiv_status, by = "SampleID")
+
+#add a new hiv variable
+# zero means hiv positive
+
+dat_for_analysis10 <- prob_inf_frame10 %>%
+  mutate(my_hiv = hivfinal_new - 1) %>%
+  filter(!is.na(my_hiv))
+
+dat_for_vis10 <- prob_inf_frame10 %>%
+  mutate(my_hiv = hivfinal_new - 1) 
+
+
+
+# write_csv(dat_for_analysis10, here::here("code", "R Code", "dat_for_analysis_snp10_allshare.csv"))
+# write_csv(dat_for_vis10, here::here("code", "R Code", "dat_forvis_snp10_allshare.csv"))
+
+
+
+# Analyze the data --------------------------------------------------------
+
+dat_for_analysis10 <- read_csv("dat_for_analysis_snp10_allshare.csv")
+
+dim(dat_for_analysis5)[1]
+dim(dat_for_analysis10)[1]
+
+# num infection sources
+sum(dat_for_analysis5$tp_source)
+sum(dat_for_analysis10$tp_source)
+
+# same sources? 
+
+source10 <- dat_for_analysis10 %>%
+  filter(tp_source == TRUE)
+
+source5 <- dat_for_analysis5 %>%
+  filter(tp_source == TRUE) %>%
+  mutate(same = names %in% source10$names)
+
+# 11 of 13 are shared this time (using all shared)
+
+# unadjusted tp glm 
+glm5 <- glm(tp_source ~ my_hiv, data = dat_for_analysis5, family = binomial)
+OR_ci5 <- exp(confint.default(glm5))
+OR_ci5[2,2]-OR_ci5[2,1]
+
+glm10 <- glm(tp_source ~ my_hiv, data = dat_for_analysis10, family = binomial)
+OR_ci10 <- exp(confint.default(glm10))
+OR_ci10[2,2]-OR_ci10[2,1]
+
+# glm with covariates
+# vars to include 
+vars <- meta_data %>%
+  dplyr::select(SampleID, smokef1, tb_everf3, alcohol_excess, agenew, genderf1) 
+
+# new data
+# mygender, 0 is male, 1 is female
+# mysmoke 0 is yes smoke, 1 is no smoke
+# mytb is 0 is yes had tb, 1 is no did not have tb
+#myalcohol 0 is yes excess drinking, 1 is no
+new_data5 <- dat_for_analysis5 %>%
+  left_join(vars, by = "SampleID") %>%
+  mutate(mygender = genderf1 -1,
+         mysmoke = smokef1 -1,
+         mytb = tb_everf3 -1,
+         myalcohol = alcohol_excess -1)
+
+
+glm5_cov <- glm(tp_source ~ my_hiv + mygender + mysmoke + mytb + myalcohol, data = new_data5, family = binomial)
+OR_ci5_cov <- round(exp(confint.default(glm5_cov)), 2)
+point_est5 <- round(exp(summary(glm5_cov)[["coefficients"]][,1]), 2)
+names <- c("Intercept", "HIV (1 = No)", "Sex (1 = F)", "Smoke (1 = No)", "Past TB (1 = no)", "Alc (1= no)")
+
+snp5_cov_coeffs <- data.frame(cbind(names, point_est5, OR_ci5_cov)) %>%
+  rename("point_est" = 2, "CI2.5%" = 3, "CI97.5%" = 4)
+rownames(snp5_cov_coeffs) <- NULL
+
+new_data10 <- dat_for_analysis10 %>%
+  left_join(vars, by = "SampleID") %>%
+  mutate(mygender = genderf1 -1,
+         mysmoke = smokef1 -1,
+         mytb = tb_everf3 -1,
+         myalcohol = alcohol_excess -1)
+
+glm10_cov <- glm(tp_source ~ my_hiv + mygender + mysmoke + mytb + myalcohol, data = new_data10, family = binomial)
+OR_ci10_cov <- round(exp(confint.default(glm10_cov)),2)
+point_est10 <- round(exp(summary(glm10_cov)[["coefficients"]][,1]),2)
+
+snp10_cov_coeffs <- data.frame(cbind(names, point_est10, OR_ci10_cov)) %>%
+  rename("point_est" = 2, "CI2.5%" = 3, "CI97.5%" = 4)
+rownames(snp10_cov_coeffs) <- NULL
+
+# now do it for SAMBA, need to first get prob of being a source from only the sample-able population in the simulations
+# 
+# outputs_list_lastthree <- map(outputs_list, ~map(.x, ~.x %>%
+#                                             mutate(true_infector = hosts.ID %in% inf.by) %>%
+#                                             filter(last_one == TRUE | last_two == TRUE | last_three == TRUE)))
+# 
+# outputs_frames_lastthree <- map(outputs_list_lastthree, ~bind_rows(., .id = "sim"))
+# 
+# big_frame_lastthree <- outputs_frames_lastthree %>% bind_rows(.id = "simsim")
+# 
+# avg_prob_source <- big_frame_lastthree %>%
+#                    group_by(simsim, sim) %>%
+#                    summarise(num_cases = n(),
+#                              percent_source = sum(true_infector)/num_cases) %>%
+#                    ungroup() %>%
+#                    summarise(
+#                      mean_percent_source = mean(percent_source)
+#                    )
+
+# getting 0.336 right now
+
+
+# snp5
+SAMBA_sens5 <- sensitivity(as.numeric(dat_for_analysis5$tp_source),
+                           X = dat_for_analysis5$my_hiv,
+                           0.69,
+                           r = NULL,
+                           weights = NULL) %>%
+  pluck(1)
+
+
+SAMBA_res5 <- approxdist(as.numeric(dat_for_analysis5$tp_source),
+                         dat_for_analysis5$my_hiv,
+                         SAMBA_sens5) %>%
+  bind_rows(.id = "sim") %>%
+  spread(sim, Z) %>%
+  mutate(CI_Low = param - 1.96 * sqrt(variance),
+         CI_High = param + 1.96 * sqrt(variance),
+         OR = exp(param),
+         OR_Low = exp(CI_Low),
+         OR_High = exp(CI_High))
+
+# snp10
+SAMBA_sens10 <- sensitivity(as.numeric(dat_for_analysis10$tp_source),
+                            X = dat_for_analysis10$my_hiv,
+                            0.69,
+                            r = NULL,
+                            weights = NULL) %>%
+  pluck(1)
+
+SAMBA_res10 <- approxdist(as.numeric(dat_for_analysis10$tp_source),
+                          dat_for_analysis10$my_hiv,
+                          SAMBA_sens10) %>%
+  bind_rows(.id = "sim") %>%
+  spread(sim, Z) %>%
+  mutate(CI_Low = param - 1.96 * sqrt(variance),
+         CI_High = param + 1.96 * sqrt(variance),
+         OR = exp(param),
+         OR_Low = exp(CI_Low),
+         OR_High = exp(CI_High))
+
+# bayesian homebrew 
+# snp5
+model_objects5 <- list(N = dim(dat_for_analysis5)[1],
+                       z = dat_for_analysis5$tp_source,
+                       x = dat_for_analysis5$my_hiv,
+                       specificity = .97,
+                       sensitivity = .28)
+
+
+
+
+stan_fit5 <- stan(file =here::here("code", "R Code", "misclass_logistic_regression.stan"),
+                  data = model_objects5,
+                  seed = 45,
+                  iter = 2000,
+                  chains = 4)
+rstan::traceplot(stan_fit5, pars = "lp__")
+
+summary_beta15 <- stan_fit5 %>%
+  spread_draws(beta1) %>%
+  mean_qi() %>%
+  mutate(
+    OR = exp(beta1),
+    OR_Low = exp(.lower),
+    OR_high =exp(.upper)
+  )
+
+# snp10
+model_objects10 <- list(N = dim(dat_for_analysis10)[1],
+                        z = dat_for_analysis10$tp_source,
+                        x = dat_for_analysis10$my_hiv,
+                        specificity = .97,
+                        sensitivity = .28)
+
+
+
+
+stan_fit10 <- stan(file =here::here("code", "R Code", "misclass_logistic_regression.stan"),
+                   data = model_objects10,
+                   seed = 45,
+                   iter = 2000,
+                   chains = 4)
+rstan::traceplot(stan_fit10, pars = "lp__")
+
+summary_beta110 <- stan_fit10 %>%
+  spread_draws(beta1) %>%
+  mean_qi() %>%
+  mutate(
+    OR = exp(beta1),
+    OR_Low = exp(.lower),
+    OR_high =exp(.upper)
+  )
+
